8a8d8ba6a0c73dc24814bc04c3ce6b5e44437f99
zianchuan <zianchuan2000@gmail.com>
2021-12-04 16:56:26 +0800

    doing

--- app/src/main/java/com/example/fitnessapplication/LoginActivity.java
+++ app/src/main/java/com/example/fitnessapplication/LoginActivity.java
@@ -2,13 +2,31 @@ package com.example.fitnessapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button callSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN) ;
         setContentView(R.layout.activity_login);
+
+        callSignUp = findViewById(R.id.signUphere);
+
+        callSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
